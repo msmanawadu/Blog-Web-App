6118613f31694de78148788d997d8ee0732273ba
msmanawadu <msmanawadu@gmail.com>
2021-09-17 09:42:52 +0530

    Add Comments to app.js

--- app.js
+++ app.js
@@ -7,16 +7,21 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+//Express Setup
 const app = express();
 
+//EJS Setup
 app.set('view engine', 'ejs');
 
+//POST Request Body-Parsing
 app.use(express.urlencoded({
   extended: true
 }));
 
+//Static File Rendering
 app.use(express.static('public'));
 
+//GET Request to '/'
 app.get('/', function (req, res) {
   res.render('home', {
     homeContent: homeStartingContent
@@ -33,9 +38,7 @@ app.get('/', function (req, res) {
 
 
 
-
-
-
+//Setting up the Server
 app.listen(PORT, function () {
   console.log(`Server is listening on port ${PORT}`);
 });
